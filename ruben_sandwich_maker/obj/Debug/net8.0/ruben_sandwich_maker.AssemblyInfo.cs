191839d0ed13cb793f80519bbd3857a7c373881c
Benjamin Rogers <80236080+BenjaminRogersSeng@users.noreply.github.com>
2024-09-26 10:34:57 +1000

    Done

--- ruben_sandwich_maker/obj/Debug/net8.0/ruben_sandwich_maker.AssemblyInfo.cs
+++ ruben_sandwich_maker/obj/Debug/net8.0/ruben_sandwich_maker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ruben_sandwich_maker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73067f5fa57fb51ca0a72e8425fb7c58c77872c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c61abb11cf29b0d33854f900b3f5d3286fc7462")]
 [assembly: System.Reflection.AssemblyProductAttribute("ruben_sandwich_maker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ruben_sandwich_maker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
